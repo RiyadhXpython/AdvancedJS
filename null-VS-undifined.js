95d9477e2c05534463c051fb81cec06c139a28c8
md riad mahamud sirazy <riadmahamud8@gmail.comm>
2021-02-02 00:45:12 +0800

    null-vs-undifined

---
+++ null-VS-undifined.js
@@ -0,0 +1,34 @@
+let pakhi;
+console.log(pakhi);
+
+function add (num1, num2){
+    console.log(num1 + num2);
+
+}
+
+function add(num1, num2=20){
+    console.log(num1, num2);
+
+}
+
+
+
+const result = add(20);
+console.log(result);
+
+
+
+const premik = {name: "riyadh", phone: 123456789};
+console.log(premik.gf)
+
+
+let fun = undefined;
+console.log(fun);
+
+
+
+
+
+
+
+
