289810af8d7bb3458501b8d2f06958f28da84fc1
md riad mahamud sirazy <riadmahamud8@gmail.comm>
2021-02-03 00:17:21 +0800

    finalPush

--- arrayslice.js
+++ arrayslice.js
@@ -1,7 +1,10 @@
 const nums = [1,2,3,4,5,6,7,8,9];
 const part = nums.slice(4, 8);
-// console.log(part);
+console.log(part);
 
 const removed = nums.splice(2, 6);
 console.log(removed);
-console.log(nums);
+// console.log(nums);
+
+console.log (together) = nums.join("heyyyyy")
+console.log(together);
---
+++ break.js
@@ -0,0 +1,11 @@
+const nums = [1,2,3,4,5,6,7,8,9];
+for (let i = 0; i < nums.length; i++) {
+  const element = nums[i];
+  console.log(nums[i]);
+  if (nums[i]>6) {
+      break;
+      
+  }
+    }
+
+    
